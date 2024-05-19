636dc2ba273f467de3112229654559fdd439c12d
RoktavorGithub <95968357+RoktavorGithub@users.noreply.github.com>
2024-05-19 17:39:03 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const PREFIX = ["p%"];
-export const BOT_TOKEN = "MTIyNTk3MzkzOTgyNjcyMDc2OA.GflwDh.gM0Wm_NvZb3VJZ38-wK3kQFTZYLTLds4PnrYq8"
+export const BOT_TOKEN = ""
 export const OWNER_IDS = ["868780328364412960"];
